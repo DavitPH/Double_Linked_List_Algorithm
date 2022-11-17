ec8e0f7f70d5418072dbc2a70c5e02329705ef56
DavitPH <davitph0@gmail.com>
2022-11-17 16:42:15 +0700

    membuat method addNonte yg didalamnya ada fungsi untuk memasukan role dan nama siswa

--- Double_Linked_List_Algorithm/Program.cs
+++ Double_Linked_List_Algorithm/Program.cs
@@ -25,8 +25,23 @@ namespace Double_Linked_List_Algorithm
         {
             START = null;
         }
+        public void addNode()
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = (Console.ReadLine());
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+
+
+        }
+
     }
-        class Program
+    class Program
     {
         static void Main(string[] args)
         {
